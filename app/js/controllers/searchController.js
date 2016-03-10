4ccbcef77de096812df0b8ddc3a8d13c90e077cc
ViolaCrellin <viola.crellin@gmail.com>
2016-03-10 15:05:42 +0000

    Adding search

--- app/js/controllers/searchController.js
+++ app/js/controllers/searchController.js
@@ -4,16 +4,18 @@ githubApiApp.controller("searchController", function($scope, $http){
     $scope.userNotFound = false;
     $scope.loaded = false;
 
-    $http.get("https://api.github.com/users/" + $scope.username)
+    $http.get("https://api.github.com/search/users?q=" + $scope.username)
       .success(function (data) {
-        if (data.name === "") data.name = data.login;
-        $scope.user = data;
+        if (data.total_count > 0)
+        // data.name = data.login;
+        $scope.users = data.items;
         $scope.loaded = true;
       })
       .error(function () {
         $scope.userNotFound = true;
       });
 
+// data.items[1].login
 
 
   };
