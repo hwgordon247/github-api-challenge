ec53f11dd80549e22ef6293acb23aec74b4e2b53
Henry Gordon <hwgordon247@gmail.com>
2016-03-11 09:42:41 +0000

    update of the files

--- test/e2e/githubApiAppSpec.js
+++ test/e2e/githubApiAppSpec.js
@@ -30,5 +30,12 @@ describe("Github Api application", function(){
      expect(element(by.id('test')).getText()).toContain('Name: Henry Gordon');
  });
 
+ it("shows how many repos the user has", function(){
+     var user = element(by.model('username')).sendKeys('hwgordon247');
+     element(by.id('ghsubmitbtn')).click();
+     element(by.id('personalInfo')).click();
+     expect(element(by.id('test')).getText()).toContain('Repos: 27');
+ });
+
 
 });
