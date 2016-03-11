e9003b29c0c50b3641efd387ec0ec578d6af36bc
Henry Gordon <hwgordon247@gmail.com>
2016-03-11 09:58:51 +0000

    can now view number of repos in more info

--- app/js/controllers/personalSearchController.js
+++ app/js/controllers/personalSearchController.js
@@ -6,7 +6,7 @@ var self = this;
     $scope.userNotFound = false;
     $scope.loadedAgain = false;
 
-    $http.get("https://api.github.com/users/" + login)
+    $http.get("https://api.github.com/users/" + login + "?access_token=3a8128552359d69803a6ae6df6bbeed9338e251d")
       .success(function (data) {
         if (data.name === "") data.name = data.login;
         $scope.specificUser = data;
